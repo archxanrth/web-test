9b3811e406d079a50b77c9bea27f5a384c9c9014
archxanrth <136441171+archxanrth@users.noreply.github.com>
2023-12-08 16:55:29 +0000

    Add files via upload

---
+++ pagesCtrlFile.js
@@ -0,0 +1,11 @@
+exports.forSaleCtrlFunction = (req, res) => {
+    res.render('index'); // Corrected to res.render
+}
+
+exports.cartCtrlFunction = (req, res) => {
+    res.render('cart'); // Corrected to res.render
+}
+
+exports.homeCtrlFunction = (req, res) => {
+    res.render('home'); // Corrected to res.render
+}
